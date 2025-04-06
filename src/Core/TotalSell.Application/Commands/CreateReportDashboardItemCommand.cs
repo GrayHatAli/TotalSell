ca5a353f5061f5c5f7db39089c6b62c632b3e4c5
GrayHatAli <grayhatali@gmail.com>
2025-04-06 12:13:54 +0330

    Rename UpdateReportDashboardItemDetailsCommand to UpdateReportDashboardItemCommand and add new properties: Description, Query, Theme, and RefreshInterval for enhanced reporting capabilities.

--- src/Core/TotalSell.Application/Commands/CreateReportDashboardItemCommand.cs
+++ src/Core/TotalSell.Application/Commands/CreateReportDashboardItemCommand.cs
@@ -2,9 +2,8 @@ using TotalSell.Application.Common;
 
 namespace TotalSell.Application.Commands;
 
-public class UpdateReportDashboardItemCommand : BaseCommand
+public class CreateReportDashboardItemCommand : BaseCommand
 {
-    public new Guid Id { get; set; }
     public Guid DashboardId { get; set; }
     public string Title { get; set; } = null!;
     public string Type { get; set; } = null!;
@@ -16,5 +15,5 @@ public class UpdateReportDashboardItemCommand : BaseCommand
     public string? Theme { get; set; }
     public int RefreshInterval { get; set; }
     public int Order { get; set; }
-    public bool IsActive { get; set; }
+    public bool IsActive { get; set; } = true;
 } 
